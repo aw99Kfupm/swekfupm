5ad87eb4c0b4950f6972dfda9f0e64d5dfb7cca4
AbulmajeedAj <AbdulmajeedAj@outlook.com>
2024-02-11 21:57:05 +0300

    adding student reading mehtod

--- Sorter.java
+++ Sorter.java
@@ -1,6 +1,29 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
 public class Sorter {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("1&2");
+
+        ArrayList<String> studentList = readStudentList("Student List.txt");
+
     }
+    public static ArrayList<String> readStudentList(String filePath) {
+        ArrayList<String> studentList = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(new File(filePath))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                studentList.add(line.trim());
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+            }
+
+        return studentList;
+    }
+
 }
