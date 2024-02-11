038fa6dbc9700149f78419142a4b475b9939ed19
AbdulmajeedAj <abdulmajeedaj@outlook.com>
2024-02-11 21:34:16 +0300

    adding reading student method

--- Sorter.java
+++ Sorter.java
@@ -2,5 +2,20 @@ public class Sorter {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("1&2");
+
+    }
+    // Ra
+    public static ArrayList<String> readStudentList(String filePath) {
+        ArrayList<String> studentList = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(new File(filePath))) {
+            while (scanner.hasNextline()) {
+                studentList.add(scanner.next());
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
+        return studentList;
     }
 }
